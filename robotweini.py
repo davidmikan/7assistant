be52d976d3112ec977c152724c4c11f4d6869cff
David Mikan <itsmikanakg@gmail.com>
2020-03-14 21:27:04 +0100

    TRAILER FILMING

--- robotweini.py
+++ robotweini.py
@@ -7,33 +7,35 @@ bot = telebot.TeleBot('928189418:AAH6SZbYLU5p7J12VhhfimKGvQZO080n0ek')
 
 triggered = ['robert', 'weini', 'weinhandl']
 ideen = []
+chid = '-1001256312641'
+bot.send_message(chid, 'Gibt\'s Probleme? Will man welche??')
 
-def error(chid, errortype):
-    if errortype == 'subject':
-        bot.send_message(chid, 'Offensichtlich hast du nichts verstanden. Für eine Liste aller Fächer schreibe /help subjects.')
-    elif errortype == 'date':
-        bot.send_message(chid, 'Nunja, ein Datum sollte man schon richtig benutzen können.')
+# def error(chid, errortype):
+#     if errortype == 'subject':
+#         bot.send_message(chid, 'Offensichtlich hast du nichts verstanden. Für eine Liste aller Fächer schreibe /help subjects.')
+#     elif errortype == 'date':
+#         bot.send_message(chid, 'Nunja, ein Datum sollte man schon richtig benutzen können.')
 
-@bot.message_handler(commands = ['help'])
-def helpcommand(message):
-    chid = message.chat.id
-    msg = assistant_help.helpmsg(message.text)
-    bot.send_message(chid, msg)
+# @bot.message_handler(commands = ['help'])
+# def helpcommand(message):
+#     chid = message.chat.id
+#     msg = assistant_help.helpmsg(message.text)
+#     bot.send_message(chid, msg)
 
-def handle_messages(messages):
-    for message in messages:
-        text = message.text
-        msg = ''
-        x = random.randint(1, 6)
-        if any(ext in text.lower() for ext in triggered) and x == 1:
-            msg = 'Gibt\'s Probleme?? \nWill man welche????'
-        if msg: bot.reply_to(message, msg)
+# def handle_messages(messages):
+#     for message in messages:
+#         text = message.text
+#         msg = ''
+#         x = random.randint(1, 6)
+#         if any(ext in text.lower() for ext in triggered) and x == 1:
+#             msg = 'Gibt\'s Probleme?? \nWill man welche????'
+#         if msg: bot.reply_to(message, msg)
         
-bot.set_update_listener(handle_messages)
+# bot.set_update_listener(handle_messages)
 
-while True:
-    try:
-        bot.polling(interval=1)
-    except Exception as e:
-        print(str(e))
-        continue
+# while True:
+#     try:
+#         bot.polling(interval=1)
+#     except Exception as e:
+#         print(str(e))
+#         continue
